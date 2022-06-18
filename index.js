1e09d64a50767b75ea4773cc1397a90a45a8b1d5
aahyder <50849507+aahyder@users.noreply.github.com>
2022-06-18 16:34:14 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const port = process.env.PORT || 4000;
 const app = express();
-const contactRouter = require('./routes/contact');
+const contactRouter = require('./routes/contact.js');
 
 // serve routes
 app.use('/', contactRouter);
